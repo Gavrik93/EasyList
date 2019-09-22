a77840a5f08f65f5642258b8d133c4dad36473d7
E.Gajvoronskij <777gava777@gmail.com>
2019-09-22 22:55:27 +0300

    add and practice some debugging flags

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -4,25 +4,34 @@ import './products.dart';
 class ProductManager extends StatefulWidget {
 
   final String startingProduct;
-  ProductManager(this.startingProduct);
+
+
+  ProductManager(this.startingProduct){
+    print('[ProductsManager Widget] Constructor');
+  }
+
+
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
+    print('[ProductsManager State] createState');
+
     return _ProductManagerState();
   }
 }
 
 class _ProductManagerState extends State<ProductManager> {
   //initialize
+
   List<String> _products = [];
   @override
   void initState() {
+    print('[ProductsManager State] initState');
     _products.add(widget.startingProduct);
     super.initState();
   }
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    print('[ProductsManager State] build()');
     return Column(
       children: [
         Container(
